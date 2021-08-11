a7a566b04baa95b127136e1a2d2c5ed941a8740e
Roberta Moura <87487658+robertalmoura@users.noreply.github.com>
2021-08-11 13:26:47 -0300

    Add files via upload

---
+++ JAVASCRIPT/Aula 1/script.js
@@ -0,0 +1,41 @@
+// Hello World:
+
+var boas_vindas = "Hello World Javascript!";
+var nome = "Roberta";
+var idade = 39;
+var altura = 1.57;
+
+document.write(boas_vindas, ". Eu sou a ", nome, " tenho ", idade, " anos de idade e ", altura, " de altura.")
+
+alert("Fim da atividade.")
+
+// Outra atividade:
+
+a = 8;
+document.write(a*= 3);
+document.write("<br/>")
+
+document.write(a/=2);
+document.write("<br/>");
+
+document.write(a%=5);
+document.write("<br/>");
+
+document.write(a+=3);
+document.write("<br/>");
+
+document.write(a-=1);
+document.write("<br/>");
+
+
+
+
+
+
+
+
+
+
+
+
+// Roberta Moura
---
+++ JAVASCRIPT/Aula 6/Exercício Dom_aula 6.js
@@ -0,0 +1,23 @@
+1.
+document.getElementById("demo").innerHTML = "Olá";
+
+2. 
+document.getElementsByTagName("p")[0].innerHTML = "Olá";
+
+3.
+document.getElementsByTagName("teste")[0].innerHTML = "Hello";
+
+4.
+document.getElementById ("image").src = "pic_montain.jpg";
+
+5.
+document.getElementById ("myText").value = "Tenha um bom dia!";
+
+6.
+document.getElementById ("Demo").style.color = "red";
+
+7.
+document.getElementById ("Demo").style.fontSize = "40px";
+
+8.
+document.getElementById ("Demo").style.display = "none";
