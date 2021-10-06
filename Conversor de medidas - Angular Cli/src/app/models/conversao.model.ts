ef8c22cc1b87c83e746100f3eb25c2130b18328b
Roberta Moura <87487658+robertalmoura@users.noreply.github.com>
2021-10-06 17:32:17 -0300

    Delete Conversor de medidas - Angular Cli directory

--- Conversor de medidas - Angular Cli/src/app/models/conversao.model.ts
+++
@@ -1,3 +0,0 @@
-export class Conversao {
-    public tipoConversao?:string;
-}
--- Conversor de medidas - Angular Cli/src/app/pipes/kilometro-para-metro.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { KilometroParaMetroPipe } from './kilometro-para-metro.pipe';
-
-describe('KilometroParaMetroPipe', () => {
-  it('create an instance', () => {
-    const pipe = new KilometroParaMetroPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
